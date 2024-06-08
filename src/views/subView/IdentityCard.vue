b5308f37902ed9f8c8f29d13a415de21a9e9f03b
Huang Zhe <3451701311@qq.com>
2024-06-09 02:24:44 +0800

    feat: 添加 loading 效果
    点击获取身份证信息后添加loading等待效果

--- src/views/subView/IdentityCard.vue
+++ src/views/subView/IdentityCard.vue
@@ -6,6 +6,7 @@ import {ElNotification} from "element-plus";
 const identity = ref<identityCard>({name: '张三', number: '123321'})
 const name = ref(identity.value.name)
 const number = ref(identity.value.number)
+const loading = ref<boolean>(false)
 
 function copyIdentityCardProp(props: keyof identityCardProps) {
   if (navigator.clipboard) {
@@ -23,18 +24,20 @@ function copyIdentityCardProp(props: keyof identityCardProps) {
 </script>
 
 <template>
-  <div>
-    <el-button type="primary" :icon="Pointer">获取随机身份证</el-button>
+  <div v-loading="loading">
     <el-form label-position="right" label-width="auto">
+      <el-form-item>
+        <el-button type="primary" :icon="Pointer" @click="getRandomIdentityCard()">获取随机身份证</el-button>
+      </el-form-item>
       <el-form-item label="姓名">
-        <el-input v-model="name">
+        <el-input v-model="name" placeholder="点击获取按钮之后生成">
           <template #append>
             <el-button @click="copyIdentityCardProp(name)">复制</el-button>
           </template>
         </el-input>
       </el-form-item>
       <el-form-item label="身份证号码">
-        <el-input v-model="number">
+        <el-input v-model="number" placeholder="点击获取按钮之后生成">
           <template #append>
             <el-button @click="copyIdentityCardProp(number)">复制</el-button>
           </template>
