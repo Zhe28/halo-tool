eae7cc3ff554570c247dd75cfeefde2b0fcfae0d
Huang Zhe <3451701311@qq.com>
2024-06-11 12:53:20 +0800

    feat: 增加https支持及调整端口配置
    1. http访问端口为 521
    2. http访问端口为 522

--- src/server/index.ts
+++ src/server/index.ts
@@ -1,18 +1,27 @@
 import koa from 'koa';
 import cros from '@koa/cors'
 import {router} from "./api/index.js";
+import * as http from "node:http";
+import * as https from "node:https";
 
-const PORT = 521
+const HTTP_PORT = 521
+const HTTPS_PORT = 522
 const server = new koa()
 
 server.use(cros())
 // server.use(logger)
 server.use(router.routes())
 
-server.listen(
-  PORT,
-  () => {
-    console.log('server has been started. the listening port is', PORT)
-  })
+// server.listen(
+//   PORT,
+//   () => {
+//     console.log('server has been started. the listening port is', PORT)
+//   })
+//
 
-
+http.createServer(server.callback()).listen(HTTP_PORT, () => {
+  console.log('server has been started. the listening port is', HTTP_PORT)
+});
+https.createServer(server.callback()).listen(HTTPS_PORT, () => {
+  console.log('server has been started. the listening port is', HTTPS_PORT)
+});
