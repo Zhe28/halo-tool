4118a8a9439767010a768b8392beca6835417407
Huang Zhe <3451701311@qq.com>
2024-06-11 13:00:20 +0800

    feat: 检测网页协议 请求对应的 api
    1. https 端口请求 522
    2. http 端口请求 521

--- src/utils/axios.ts
+++ src/utils/axios.ts
@@ -1,8 +1,19 @@
 import _axios from "axios"
 
-export const axios = _axios.create({
-  baseURL: "http://localhost:521/",
-})
+function createAxios() {
+  // 检测是否是 http 页面  https端口请求 522
+  if (window.location.protocol === "http:") {
+    return _axios.create({
+      baseURL: "https://localhost:522/",
+    })
+  }
+  // http 端口请求 521
+  return _axios.create({
+    baseURL: "http://localhost:521/",
+  })
+}
+
+export const axios = createAxios();
 
 const getIdentityCard = async () => {
   return (await axios.get("/getRandomIdentityCard")).data
