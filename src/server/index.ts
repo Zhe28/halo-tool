8ff307a0e8dcef2feef36ddc80589e468e612c62
Huang Zhe <3451701311@qq.com>
2024-06-11 16:09:37 +0800

    feat: https 添加证书支持

--- src/server/index.ts
+++ src/server/index.ts
@@ -3,9 +3,14 @@ import cros from '@koa/cors'
 import {router} from "./api/index.js";
 import * as http from "node:http";
 import * as https from "node:https";
+import {readFileSync} from "node:fs";
 
 const HTTP_PORT = 521
 const HTTPS_PORT = 522
+const sslOption = {
+  key: readFileSync("resources/ssl-cert/private_key.pem"),
+  cert: readFileSync("resources/ssl-cert/ca-cert.pem")
+}
 const server = new koa()
 
 server.use(cros())
@@ -22,6 +27,6 @@ server.use(router.routes())
 http.createServer(server.callback()).listen(HTTP_PORT, () => {
   console.log('server has been started. the listening port is', HTTP_PORT)
 });
-https.createServer(server.callback()).listen(HTTPS_PORT, () => {
+https.createServer(sslOption, server.callback()).listen(HTTPS_PORT, () => {
   console.log('server has been started. the listening port is', HTTPS_PORT)
 });
