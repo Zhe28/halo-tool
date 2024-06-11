e848571bccea58db73d757e1e11ad8526d323349
Huang Zhe <3451701311@qq.com>
2024-06-11 15:42:25 +0800

    feat: element 组件库自动引入

--- src/main.ts
+++ src/main.ts
@@ -1,16 +1,16 @@
 import {createApp} from 'vue'
 import {createPinia} from "pinia";
-import elementPlus from "element-plus"
+// import elementPlus from "element-plus"
 
 import {router} from "./router";
 
 import App from './views/Home.vue'
-import "element-plus/dist/index.css"
+// import "element-plus/dist/index.css"
 import './main.css'
 
 
 const app = createApp(App);
 app.use(createPinia())
 app.use(router)
-app.use(elementPlus)
+// app.use(elementPlus)
 app.mount('#app')
