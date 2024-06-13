22a5d41733c59c917c1773174a042cb2b360a2c3
Huang Zhe <3451701311@qq.com>
2024-06-13 14:49:30 +0800

    fix: 增加 HTTP 证书
    1. 增加 hzer.fun 证书支持

--- src/server/index.ts
+++ src/server/index.ts
@@ -1,15 +1,15 @@
 import koa from 'koa';
 import cros from '@koa/cors'
 import {router} from "./api/index.js";
-import * as http from "node:http";
-import * as https from "node:https";
+import {createServer as createHTTPServer} from "node:http";
+import {createServer as createHTTPSServer} from "node:https";
 import {readFileSync} from "node:fs";
 
 const HTTP_PORT = 521
 const HTTPS_PORT = 522
 const sslOption = {
-  key: readFileSync("resources/ssl-cert/private_key.pem"),
-  cert: readFileSync("resources/ssl-cert/ca-cert.pem")
+  cert: readFileSync("resources/ssl-cert/fullchain.pem"),
+  key: readFileSync("resources/ssl-cert/privkey.pem")
 }
 const server = new koa()
 
@@ -24,9 +24,9 @@ server.use(router.routes())
 //   })
 //
 
-http.createServer(server.callback()).listen(HTTP_PORT, () => {
+createHTTPServer(server.callback()).listen(HTTP_PORT, () => {
   console.log('server has been started. the listening port is', HTTP_PORT)
 });
-https.createServer(sslOption, server.callback()).listen(HTTPS_PORT, () => {
+createHTTPSServer(sslOption, server.callback()).listen(HTTPS_PORT, () => {
   console.log('server has been started. the listening port is', HTTPS_PORT)
 });
