3a21750b8e031c8f91a19817cbe53fab8ac982eb
Huang Zhe <3451701311@qq.com>
2024-06-17 14:25:11 +0800

    feat:  navbar 样式更改

--- src/views/subView/Header.vue
+++ src/views/subView/Header.vue
@@ -1,7 +1,6 @@
 <script setup lang="ts">
 import ToolIcon from "../../assets/ToolIcon.vue";
 import {ref} from "vue";
-import {useGlobalConfig} from "../../stores";
 
 const title = ref<string>('halo tool')
 const underline = ref(false)
@@ -9,17 +8,13 @@ const underline = ref(false)
 
 <template>
   <div class="container h-full w-full flex flex-row flex-nowrap justify-between items-center">
-    <el-link :underline=underline href="/" class="md:hidden">
-      <el-icon>
+    <el-link :underline=underline href="/">
+      <el-icon size="40">
         <ToolIcon/>
       </el-icon>
-      <h1 class='m-2'> {{ title }}</h1>
+      <div class='m-2 text-xl'> {{ title }}</div>
     </el-link>
 
-    <el-radio-group v-model="useGlobalConfig().sidebar.isCollapse">
-      <el-radio-button :value="false">expand</el-radio-button>
-      <el-radio-button :value="true">collapse</el-radio-button>
-    </el-radio-group>
   </div>
 </template>
 
