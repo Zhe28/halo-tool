814976b6c5bec58dfa8d18ef54bbcc2828a11a5c
Huang Zhe <3451701311@qq.com>
2024-06-18 16:57:01 +0800

    feat: 头部导航链接更改
    1. 修改头部导航链接为 router-link 形式

--- src/views/subView/Header.vue
+++ src/views/subView/Header.vue
@@ -3,17 +3,16 @@ import ToolIcon from "../../assets/ToolIcon.vue";
 import {ref} from "vue";
 
 const title = ref<string>('halo tool')
-const underline = ref(false)
 </script>
 
 <template>
   <div class="container h-full w-full flex flex-row flex-nowrap justify-between items-center">
-    <el-link :underline=underline href="/">
+    <router-link to="/" class="flex flex-row flex-nowrap justify-between items-center">
       <el-icon size="40">
         <ToolIcon/>
       </el-icon>
       <div class='m-2 text-xl'> {{ title }}</div>
-    </el-link>
+    </router-link>
 
   </div>
 </template>
